10074b1e0190cafc08e3205e53e04aa889a8469c
mehdi naciri <naciri2002mehdi@gmail.com>
2025-04-21 10:58:50 +0100

    New Code

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 class MyModel(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=150)
     def __str__(self):
         return self.name
